dc1dd2ee48c89b48fa980747295b535deddf2636
Wojciech Gawronski <afronski@gmail.com>
2015-09-05 10:55:53 +0200

    Flow of talk and slides.

--- in-progress/elixir-plus-phoenix@kariera-it-ktw/src/elixir_plus_phoenix_at_kariera_it_ktw_2015/core.cljs
+++ in-progress/elixir-plus-phoenix@kariera-it-ktw/src/elixir_plus_phoenix_at_kariera_it_ktw_2015/core.cljs
@@ -31,6 +31,8 @@
 (defslide whoami [_]
   [:h2 "~ $ whoami"
    [:ul.no-list
+    [:li "Companies and Logos"]
+    [:li "Quick introduction and building authority (polyglot programmer)"]
     [:li [:h6 "Twitter: " [:em "@afronski"]]]
     [:li [:h6 "Github: " [:em "afronski"]]]]])
 
@@ -47,9 +49,59 @@
   [:div
    [:h1 "Erlang poll"]
    [:ul
-    [:li "The same trick as on DWO 2015 with poll about Erlang VM."]]])
+    [:li "The same trick as on DWO 2015 with poll about Erlang VM. ;)"]]])
 
-(defslide content [_]
+(defslide why [_]
+  [:div
+   [:ul
+    [:li "Free lunch is over!"]
+    [:li "Multi and manycore is here."]
+    [:li "How hard is to build multithreaded and concurrent systems?"]
+    [:li "Leaking abstractions (Event Loop, Promises, CSP etc.)"]]])
+
+(defslide foundation [_]
+  [:div
+   [:ul
+    [:li "Erlang - 30+ years of engineering"]
+    [:li "It is impossible to talk about Elixir without talking about Erlang."]
+    [:li "BEAM ecosystem - Standing on the shoulders of giants"]
+    [:li "Why we should care about telecom? Well, it is our use case."]
+    [:li "If we have Erlang, why we need to build something else?"]]])
+
+(defslide impressions [_]
+  [:div
+   [:ul
+    [:li "Functional Language - Referential Transparency, Predictability, Immutability"]
+    [:li "Developer Happiness"]
+    [:li "'I loved things which I have seen, and I hated things there weren't there'"]
+    [:li "Metaprogramming - Hygenic Macros"]
+    [:li "Last, but not least - Syntax"]]])
+
+(defslide ok-but-why-not-ruby-nor-rails [_]
+  [:div
+   [:ul
+    [:li "Meh, we have got it already (Django, Rails etc.)"]
+    [:li "No, you don't. How hard is to build scalable web app in Rails?"]
+    [:li "How hard is to do a real-time communication in those frameworks?"]
+    [:li "How hard is to distribute them across multiple machines?"]]])
+
+(defslide microservices-are-the-answer [_]
+  [:div
+   [:ul
+    [:li "Microservices for the rescue!"]
+    [:li "Yes, but ... what about operational costs? Do you have DevOps culture?"]
+    [:li "Instead look at the highest level of Erlang/Elixir systems."]
+    [:li "We have applications. It means that Erlang was built 30 years ago with microservices in mind."]
+    [:li "Baked in Concurrency (in the langauge) and Distribution (in the platform)."]]])
+
+(defslide what-is-in-the-box [_]
+  [:div
+   [:ul
+    [:li "Who doesn't like physics?"]
+    [:li "How important physics is for the computer science? A lot - entrophy, information equations etc."]
+    [:li "Actor Model is a physical world analogy for concurrency inside computers."]]])
+
+(defslide demos [_]
   [:div
     [:h1 "Case Studies"]
     [:ul
@@ -59,45 +111,35 @@
      [:li "Free lunch is over! - Concurrency, Distribution OOTB"]
      [:li "Stable foundation! - 30+ years of experience"]]])
 
-(defslide other [_]
+(defslide elixir-and-phoenix [_]
   [:div
    [:h3 "Topics"]
    [:ul.micro
-          [:li "What is Elixir?"]
-          [:li "Standing on the shoulders of giants"]
-          [:li "Elevator pitch for Erlang VM / Elixir"
-             [:ul
-               [:li "Actor Model"]
-               [:li "Concurrency Oriented language (analogy to physics"]
-               [:li "Baked-in Distribution"]
-               [:li "30+ years of experience"]
-               [:li "Functional Language - Referential Transparency, Predictability, Immutability"]
-               [:li "Hygenic Macros"]]]
-          [:li "What is Phoenix?"]
-          [:li "Elevator pitch for Phoenix"
-             [:ul
-               [:li "Applications vs. Microservices"]
-               [:li "Microservices done good in a 30+ years"]
-               [:li "Easy for developers (Speed)"]
-               [:li "Easy to deploy (Heroku Case)"]
-               [:li "You are not sacrificing performance"]]]
-          [:li "Stand-back a little bit"
-             [:ul
-               [:li "Web world analogy to Telecom world"]]]
-          [:li "Project kick-off"
-             [:ul
-               [:li "asciinema FTW!"]]]
-          [:li "Creating a chat (very easy)"]
-          [:li "What besides Phoenix?"
-             [:ul
-               [:li "Ecto"]
-               [:li "Client libraries for C#, Java, ObjC and Swift"]]]
-          [:li "What's next?"
-             [:ul
-               [:li "Books (Programming Elixir, Introducing Elixir, Elixir Metaprogramming)"]
-               [:li "Books (Learn You Some Erlang!)"]
-               [:li "Blogs"]
-               [:li "Learn by doing it"]]]]])
+    [:li "What is Elixir?"]
+    [:li "What is Phoenix?"]
+    [:li "Elevator pitch for Phoenix"
+     [:ul]
+     [:li "Easy for developers (Speed)"]
+     [:li "Easy to deploy (Heroku Case)"]
+     [:li "You are not sacrificing performance"]]]
+   [:li "Project kick-off"
+    [:ul
+     [:li "asciinema FTW!"]]]
+   [:li "Creating a chat (very easy)"]
+   [:li "What besides Phoenix?"
+    [:ul
+     [:li "Ecto"]
+     [:li "Client libraries for C#, Java, ObjC and Swift"]]]])
+
+(defslide whats-next [_]
+   [:div
+    [:h2 "What's next?"]
+    [:ul
+     [:li "Books (Programming Elixir, Introducing Elixir, Elixir Metaprogramming)"]
+     [:li "Books (Learn You Some Erlang!)"]
+     [:li "Silesian BEAMers - we're starting!"]
+     [:li "Blogs"]
+     [:li "Learn by doing it"]]])
 
 (defn ^:export main []
   (f/presentation-init app-state)
